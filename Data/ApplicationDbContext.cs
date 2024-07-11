b1664700f40b5d43ab837f4a36cb645b90ae5ef6
Khaled-144 <116170508+Khaled-144@users.noreply.github.com>
2024-07-11 12:26:37 +0300

    App is working, and all packages are installed.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CharityProject.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSet properties for your entities
+        //public DbSet<Employee> Employees { get; set; }
+    }
+}
