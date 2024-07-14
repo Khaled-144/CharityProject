947c80dfa80e10aa4b5428f1a2e09c34dce3042c
Khaled-144 <116170508+Khaled-144@users.noreply.github.com>
2024-07-14 12:22:36 +0300

    added models from the database tables.

---
+++ Models/Letter.cs
@@ -0,0 +1,19 @@
+﻿namespace CharityProject.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Letter
+    {
+        [Key]
+        public int LetterId { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime Date { get; set; }
+        public int FromEmpId { get; set; }
+        public int? ToEmpId { get; set; }
+        public string? Files { get; set; }
+        public int DepartmentId { get; set; }
+    }
+
+}
---
+++ Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿namespace CharityProject.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        public string Type { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime? CloseDate { get; set; }
+        public string Status { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public string? Files { get; set; }
+        public int FromEmpId { get; set; }
+        public int? ToEmpId { get; set; }
+        public int DepartmentId { get; set; }
+    }
+
+}
