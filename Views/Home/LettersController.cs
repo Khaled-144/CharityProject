546045a90fc0526f9b94196deb96c65ba725212d
Khaled-144 <116170508+Khaled-144@users.noreply.github.com>
2024-07-16 10:45:15 +0300

    letters MVC is added

---
+++ Views/Home/LettersController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CharityProject.Data;
+using CharityProject.Models;
+
+namespace CharityProject.Views.Home
+{
+    public class LettersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public LettersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Letters
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Letters.ToListAsync());
+        }
+
+        // GET: Letters/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var letter = await _context.Letters
+                .FirstOrDefaultAsync(m => m.letters_id == id);
+            if (letter == null)
+            {
+                return NotFound();
+            }
+
+            return View(letter);
+        }
+
+        // GET: Letters/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Letters/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("letters_id,title,description,date,from_emp_id,to_emp_id,files,departement_id")] Letter letter)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(letter);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(letter);
+        }
+
+        // GET: Letters/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var letter = await _context.Letters.FindAsync(id);
+            if (letter == null)
+            {
+                return NotFound();
+            }
+            return View(letter);
+        }
+
+        // POST: Letters/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("letters_id,title,description,date,from_emp_id,to_emp_id,files,departement_id")] Letter letter)
+        {
+            if (id != letter.letters_id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(letter);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!LetterExists(letter.letters_id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(letter);
+        }
+
+        // GET: Letters/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var letter = await _context.Letters
+                .FirstOrDefaultAsync(m => m.letters_id == id);
+            if (letter == null)
+            {
+                return NotFound();
+            }
+
+            return View(letter);
+        }
+
+        // POST: Letters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var letter = await _context.Letters.FindAsync(id);
+            if (letter != null)
+            {
+                _context.Letters.Remove(letter);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool LetterExists(int id)
+        {
+            return _context.Letters.Any(e => e.letters_id == id);
+        }
+    }
+}
