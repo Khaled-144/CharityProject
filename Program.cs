3f756eaf99002d8e70d417d1cb444c27db497aa4
Khaled-144 <116170508+Khaled-144@users.noreply.github.com>
2024-07-18 19:18:58 +0300

    سويت خدمة طباعة افادة بالراتب لكن فيها بعض المشاكل مثل: الكتابة تجي فصفحة ثانية لحالها. بس أهم شي الكاتبة بالعربية ممتازة. وأضفت بعض المجلدات والملفات اللي احتجتها.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 using CharityProject.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using PdfSharp.Fonts;
+using IronPdf;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -39,5 +42,11 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+
+// Assign custom font resolver
+GlobalFontSettings.FontResolver = new CustomFontResolver();
+
+
+IronPdf.License.LicenseKey = "IRONSUITE.Z1EX3VY5Z.RELAY.FIREFOX.COM.7292-658CF33405-BZF3RPP3HBLVOH-3GR3YXCKLQZO-TC5LPG4R4Y3I-HEWQ4YQSSFMO-TPO3HMFJU6ZK-P67MZSAWCZX5-MRWIUO-TRNLFLGTPNGNEA-DEPLOYMENT.TRIAL-F42NKS.TRIAL.EXPIRES.17.AUG.2024";
 
 app.Run();
