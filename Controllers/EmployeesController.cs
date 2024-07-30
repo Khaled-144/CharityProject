6a09a9b750d58c7e09d871a5d65d9a0cea130bbd
Abdullah-TUS <xxaboodi2212@gmail.com>
2024-07-30 18:10:47 +0300

    completed 90% of EmployeesController

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -1,7 +1,8 @@
 ﻿using CharityProject.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-
+using CharityProject.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 namespace CharityProject.Controllers
 {
     public class EmployeesController : Controller
@@ -18,6 +19,9 @@ namespace CharityProject.Controllers
         {
             return View();
         }
+
+        // GET Actions -------------------------------------------------------- no details needed ( all used thrugh partial view )
+
         public async Task<IActionResult> Transactions()
         {
             return View(await _context.Transactions.ToListAsync());
@@ -41,6 +45,94 @@ namespace CharityProject.Controllers
             var letters = await _context.Letters.ToListAsync();
             return PartialView("_getAllLetters", letters);
         }
+
+        // Create Actions  --------------------------------------------------------
+
+        public IActionResult Create_Transaction()
+        {
+            return View();
+        }
+
+        // POST: Transactions/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create_Transaction([Bind("create_date,close_date,title,description,files,from_emp_id,to_emp_id,department_id")] Transaction transaction)
+        {
+            if (ModelState.IsValid)
+            {
+                if (transaction.create_date == null)
+                {
+                    transaction.create_date = DateTime.Now;
+                }
+
+                transaction.status = "مرسلة";  // Set default status value
+                _context.Add(transaction);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Transactions));
+            }
+            return View(transaction);
+        }
+
+        public IActionResult Create_Holiday()
+        {
+            ViewData["holiday_id"] = new SelectList(_context.Holidays, "holiday_id", "holiday_id");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create_Holiday([Bind("title,description,duration,emp_id,start_date,end_date,files,holiday_id")] HolidayHistory holidayHistory)
+        {
+
+            holidayHistory.creation_date = DateOnly.FromDateTime(DateTime.Now); // Set to current date
+            holidayHistory.status = "مرسلة"; // Set default status
+            _context.Add(holidayHistory);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create_Letter([Bind("title,description,type,from_emp_id,to_emp_id,files")] Letter letter)
+        {
+            if (ModelState.IsValid)
+            {
+                letter.date = DateTime.Now; // Set the current date
+                letter.departement_id = 3;
+                _context.Add(letter);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(letter);
+        }
+
+
+        // Update Actions --------------------------------------------------------
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateTransactionStatus(int transaction_id)
+        {
+            var transaction = await _context.Transactions.FindAsync(transaction_id);
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            // Update the status to "Closed"
+            transaction.status = "منهاة";
+            transaction.close_date = DateTime.Now;
+
+            // Save changes to the database
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        // Delete Actions --------------------------------------------------------
 
     }
 }
